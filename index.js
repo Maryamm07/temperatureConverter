57b083dcc2ee87509ed0745e89a9b58fe744629f
Maryamm07 <118045647+Maryamm07@users.noreply.github.com>
2024-01-07 17:42:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const celcius = document.querySelector('#celcius > input');
+const fahrenheit = document.querySelector('#fahrenheit > input');
+const kelvin = document.querySelector('#kelvin > input');
+
+function roundNum(num){
+    return Math.round(num*100)/100;
+}
+function cToFandK(){
+    const cTemp = parseFloat(celcius.value);
+    const fTemp = (cTemp * (9/5)) + 32;
+    const kTemp = cTemp + 273.15;
+    fahrenheit.value = roundNum(fTemp);
+    kelvin.value = roundNum(kTemp);
+}
+function fToCandK(){
+    const fTemp = parseFloat(fahrenheit.value);
+    const cTemp = (fTemp - 32) * (5/9);
+    const kTemp = (fTemp + 459.67) * 5/9;
+    celcius.value = roundNum(cTemp);
+    kelvin.value = roundNum(kTemp);
+}
+function kToCandF(){
+    const kTemp = parseFloat(kelvin.value);
+    const cTemp = kTemp - 273.15;
+    const fTemp = 9/5 * (kTemp - 273) + 32;
+    celcius.value = roundNum(cTemp);
+    fahrenheit.value = roundNum(fTemp); 
+}
+function main(){
+    celcius.addEventListener('input', cToFandK);
+    fahrenheit.addEventListener('input', fToCandK);
+    kelvin.addEventListener('input', kToCandF);
+}
+
+main();
